027be92025876a2a16ce6b95f68dbfa3d98df997
mohamedeliwa <mohamed.av11@gmail.com>
2020-01-10 18:18:43 +0200

    addind Home.js file and deleting app.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
